24af3f21e6b47d3d6ef97869f52b5b92a777cc06
roziqov1011 <roziqov1011@gmail.com>
2021-10-03 20:17:33 +0500

    initil commit

---
+++ js/index.js
@@ -0,0 +1,77 @@
+// Saqlangan kontaktlarni arrayga saqlash uchun
+var contacts = [];
+
+// HTML elementlarni JSga chaqirib olish
+var elNewContactForm = $_(".js-new-contact-form");
+var elNewContactNameInput = $_(".js-new-contact-form__name-input", elNewContactForm);
+var elNewContactSurnameInput = $_(".js-new-contact-form__surname-input", elNewContactForm);
+var elNewContactNumberInput = $_(".js-new-contact-form__contact-input", elNewContactForm);
+var elNewContactRelationshipInput = $_( ".js-new-contact-form__relationship-input", elNewContactForm);
+var elContacts = $_(".contacts");
+
+var createContactObject = function (name, surname, number, relationship) {
+  return {
+    name,
+    surname,
+    number,
+    relationship,
+  };
+};
+
+var renderContactsList = function () {
+  elContacts.innerHTML = "";
+  // for (var i = 0; i < contacts.length; i++)
+  contacts.forEach(function (contact, i) {
+    var newLiContact = createElement("li", "contact__item list-group-item style");
+    var newDivFullname = createElement("div", "contact__fullname",`${contacts[i].name} ${contacts[i].surname}`);
+    var newDivRelationship = createElement("div", "contact__relationship small", contacts[i].relationship);
+    var newDivNumber = createElement("a", "contact__phone-number", contacts[i].number);
+    newDivNumber.href = `tel:${contacts[i].number}`;
+
+    var newElBtn = createElement("button", "btn btn-light js-btn", "x");
+    newElBtn.value = i;
+
+    newLiContact.appendChild(newDivFullname);
+    newLiContact.appendChild(newDivRelationship);
+    newLiContact.appendChild(newDivNumber);
+    newLiContact.appendChild(newElBtn);
+
+    elContacts.appendChild(newLiContact);
+
+    newElBtn.addEventListener("click", function (){
+      contacts.splice(parseInt (this.value), 1);
+      renderContactsList();
+    });
+  }
+)};
+
+
+
+
+var clearInputFields = function () {
+  elNewContactNameInput.value = "";
+  elNewContactSurnameInput.value = "";
+  elNewContactNumberInput.value = "";
+  elNewContactRelationshipInput.value = "";
+
+  elNewContactNameInput.focus();
+};
+
+elNewContactNameInput.focus();
+
+elNewContactForm.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+
+  var name = elNewContactNameInput.value.trim();
+  var surname = elNewContactSurnameInput.value.trim();
+  var number = elNewContactNumberInput.value.trim();
+  var relationship = elNewContactRelationshipInput.value.trim();
+
+  contacts.push(createContactObject(name, surname, number, relationship));
+
+  clearInputFields();
+  renderContactsList();
+});
+
+
+
